a03867ee2948b734d9336e402d67a229a9f8efe5
Muhammad Ferdian Iqbal <muhammadferdianiqbal@gmail.com>
2026-06-12 20:52:15 +0700

    feat: update prisma setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/config/prisma/schema.prisma",
+  schema: "src/config/prisma/schema",
   migrations: {
     path: "src/config/prisma/migrations",
   },
