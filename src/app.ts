fc05f74f73ecc8c57be76c40d9611196a43eee5b
Muhammad Ferdian Iqbal <muhammadferdianiqbal@gmail.com>
2026-06-17 00:20:17 +0700

    refactor(app): chain middleware registrations in Express app setup

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { globalErrorHandler } from "@/middlewares/error.middleware";
 import compression from "compression";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -5,23 +6,21 @@ import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 import routes from "./routes";
-import { globalErrorHandler } from "@/middlewares/error.middleware";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(helmet());
-app.use(cors());
-app.use(cookieParser()); // Mount cookie-parser to parse request cookies
-app.use(express.urlencoded({ extended: false }));
-app.use(compression());
-app.use(express.json());
+app
+  .use(helmet())
+  .use(cors())
+  .use(cookieParser())
+  .use(express.urlencoded({ extended: false }))
+  .use(compression())
+  .use(express.json())
 
-app.use("/api", routes);
-
-// Global Error Handler (must be mounted last)
-app.use(globalErrorHandler);
+  .use("/api", routes)
+  .use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
